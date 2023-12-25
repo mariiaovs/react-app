ebf47e93707326b3f82d597419c2708b5d4e885f
Mariia Posselt <maruw@ya.ru>
2023-12-25 14:42:11 +0100

    style active menu button

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,18 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 function Navigation() {
+  const [activeLink, setActiveLink] = useState("start");
+
   return (
-    <nav className="h-[50px] flex justify-between px-5 bg-blue-500 font-white items-center">
+    <nav className="h-[50px] flex justify-between px-5 bg-blue-400 font-white items-center">
       <span className="font-bold">Online-Shop</span>
       <span>
-        <Link to="/" className="mr-2 p-5">
+        <Link
+          to="/"
+          onClick={() => setActiveLink("start")}
+          className={
+            activeLink === "start"
+              ? "border-b-4 border-white mr-2 px-3 py-1"
+              : "mr-2 px-3 py-1"
+          }
+        >
           Start
         </Link>
-        <Link to="/products" className="mr-2 p-5">
+        <Link
+          to="/products"
+          onClick={() => setActiveLink("products")}
+          className={
+            activeLink === "products"
+              ? "border-b-4 border-white mr-2 px-3 py-1"
+              : "mr-2 px-3 py-1"
+          }
+        >
           Products
         </Link>
-        <Link to="/about" className="mr-2 p-5">
+        <Link
+          to="/about"
+          onClick={() => setActiveLink("about")}
+          className={
+            activeLink === "about"
+              ? "border-b-4 border-white mr-2 px-3 py-1"
+              : "mr-2 px-3 py-1"
+          }
+        >
           About
         </Link>
       </span>
--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 
 function AboutPage() {
-  
   return (
     <div>
-        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aspernatur cum at cumque laboriosam itaque necessitatibus dignissimos aliquid beatae facere sapiente quis quod sint, reiciendis ex, sit tempore voluptatum earum illo!</p>
+      <p className="text-center m-5">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aspernatur cum
+        at cumque laboriosam itaque necessitatibus dignissimos aliquid beatae
+        facere sapiente quis quod sint, reiciendis ex, sit tempore voluptatum
+        earum illo!
+      </p>
     </div>
   );
 }
