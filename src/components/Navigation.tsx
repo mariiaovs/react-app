b354d2b394a5682c800cfc5d6a9b234162bac6d1
Mariia Posselt <maruw@ya.ru>
2023-12-25 13:46:21 +0100

    change styles of navigation

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -6,13 +6,15 @@ function Navigation() {
     <nav className="h-[50px] flex justify-between px-5 bg-blue-500 font-white items-center">
       <span className="font-bold">Online-Shop</span>
       <span>
-        <Link to="/" className="mr-2">
+        <Link to="/" className="mr-2 p-5">
           Start
         </Link>
-        <Link to="/products" className="mr-2">
+        <Link to="/products" className="mr-2 p-5">
           Products
         </Link>
-        <Link to="/about">About</Link>
+        <Link to="/about" className="mr-2 p-5">
+          About
+        </Link>
       </span>
     </nav>
   );
