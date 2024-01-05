c48adb0b744838313bdf8223fc69eead398b7659
Mariia Posselt <maruw@ya.ru>
2024-01-05 13:37:14 +0100

    change styles

--- src/pages/StartPage.tsx
+++ src/pages/StartPage.tsx
@@ -12,7 +12,7 @@ function StartPage() {
       <h1 className="text-5xl text-center font-bold mx-20 my-20">
         Welcome to our shop!
       </h1>
-      <div className="flex gap-5 mx-5">
+      <div className="flex gap-5 justify-center">
         {loading && <Loader />}
         {error && <ErrorMessage errorMessage={error} />}
         {categories.map((category, index) => (
